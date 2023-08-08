fb6257f9c2a558840069ce7eecf469790e147d65
ChimpChonk <sanjit.poudel28@gmail.com>
2023-08-08 08:03:54 +0200

    Country interface and repo added

---
+++ WebApiLearn/WebApiLearning/WebApiLearning/Interface/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApiLearning.Models;
+
+namespace WebApiLearning.Interface
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int ownerId);
+        ICollection<Owner> GetOwnersFromCountry(int ownerId);
+        bool countryExists(int id);
+    }
+}
