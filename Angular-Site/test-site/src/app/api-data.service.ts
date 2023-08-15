bbd3b44eff5789a509f5f3bf76578e04438db54a
ChimpChonk <sanjit.poudel28@gmail.com>
2023-08-15 13:54:53 +0200

    shit is broken

--- Angular-Site/test-site/src/app/api-data.service.ts
+++ Angular-Site/test-site/src/app/api-data.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiDataService {
   private apiUrl = 'https://localhost:7246/api/';
-  endpoint: string = '';
+
   
   constructor(private http: HttpClient) { }
 
-  getData(endpoint): Observable<any>{
-    return this.http.get<any>(this.apiUrl + endpoint);
+  getData(endpoint: string): Observable<any>{
+    return this.http.get<any>(this.apiUrl + endpoint).pipe(retry(3));
+
+  }
+
+  getDataById(endpoint: string, id: number): Observable<any>{
+    return this.http.get<any>(this.apiUrl + endpoint + '/' + id).pipe(retry(3));
+  }
+
+  getDataByIdAndEndpoint(endpoint: string, id: number, endpoint2: string): Observable<any>{
+    return this.http.get<any>(this.apiUrl + endpoint + '/' + id + '/' + endpoint2).pipe(retry(3));
   }
 }
--- Angular-Site/test-site/src/app/cardcontainer/cardcontainer.component.ts
+++ Angular-Site/test-site/src/app/cardcontainer/cardcontainer.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiDataService } from '../api-data.service';
 
+
+interface Rating {
+  id: number;
+  title: string;
+  text: string;
+  rating: number;
+}
+
+interface Data {
+  id: number;
+  name: string;
+  birthDate: string;
+  reviews: Rating[];
+  // ... other properties ...
+}
+
 @Component({
   selector: 'app-cardcontainer',
   templateUrl: './cardcontainer.component.html',
   styleUrls: ['./cardcontainer.component.css']
 })
-export class CardcontainerComponent implements OnInit {
-  data: any;
-  endpoint: string = 'pokemon';
 
-  constructor(private apiDataService: ApiDataService){}
+export class CardcontainerComponent implements OnInit {
+  data: Data[] = [];
+  endpoint: string = 'pokemon';
+  endpoint2: string = '';
+  idData: Data[] = [];
+  idAndEndpointData: Rating[] = [];
+
+  constructor(private apiDataService: ApiDataService) {}
 
   ngOnInit(): void {
-      this.fetchData();
+    this.fetchData();
   }
 
   fetchData(): void {
     this.apiDataService.getData(this.endpoint).subscribe(
-      (response) => {
+      (response: Data[]) => {
         this.data = response;
       },
       (error) => {
@@ -27,4 +47,26 @@ export class CardcontainerComponent implements OnInit {
     );
   }
 
+  fetchDataById(id: number): void {
+    this.apiDataService.getDataById(this.endpoint, id).subscribe(
+      (response: Data[]) => {
+        this.idData = response;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
+
+  fetchDataByIdAndEndpoint(id: number, endpoint2: string): void {
+    this.apiDataService.getDataByIdAndEndpoint(this.endpoint, id, endpoint2).subscribe(
+      (response: Rating[]) => {
+        this.idAndEndpointData = response;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
 }
+
